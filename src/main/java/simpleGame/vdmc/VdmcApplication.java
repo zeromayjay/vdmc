2513905bd29c3241ccef1913eb29ca5724679f97
zeromayjay <mai.hoang@hotmail.de>
2021-09-26 13:45:35 +0200

    Initial Commit: Basic structure and simple shape classes. Spring Boot Template with Gradle

---
+++ src/main/java/simpleGame/vdmc/VdmcApplication.java
@@ -0,0 +1,13 @@
+package simpleGame.vdmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simpleGame/vdmc/VdmcApplicationTests.java
@@ -0,0 +1,13 @@
+package simpleGame.vdmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
