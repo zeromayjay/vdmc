4217d4a86540ce25ae1163d0a8f786f19d4655b9
zeromayjay <mai.hoang@hotmail.de>
2021-10-15 16:36:24 +0200

    Add Migration with Liquibase, docker compose file

--- src/main/java/simpleGame/vdmc/VdmcApplication.java
+++ src/main/java/simpleGame/vdmc/VdmcApplication.java
@@ -1,13 +1,38 @@
 package simpleGame.vdmc;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class VdmcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VdmcApplication.class, args);
+
+		SpringApplication application = new SpringApplication(VdmcApplication.class);
+		//application.setBannerMode(Banner.Mode.OFF);
+		application.run(args);
+
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
